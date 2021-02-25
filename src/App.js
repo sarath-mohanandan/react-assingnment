cab0f46451a1090ba014d9fc7b8fce2882589bb3
Sarath Mohanandan <sarath.vm@fingent.com>
2021-02-25 15:44:30 +0530

    task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { Container, Form, Row, Table, Button, Col } from "react-bootstrap";
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		async function fetchData() {
+			const response = await fetch(
+				"https://jsonplaceholder.typicode.com/posts"
+			);
+			setPosts(response);
+		}
+		fetchData();
+	}, []);
+	return (
+		<div className="App">
+			<Container>
+				<h1>Posts</h1>
+				<Row className="mb-1">
+					<Col>
+						<Form.Control type="text" placeholder="Search" />
+					</Col>
+					<Col>
+						<Button>Search</Button>
+					</Col>
+				</Row>
+				<Table striped bordered hover size="sm">
+					<thead>
+						<tr>
+							<th>#</th>
+							<th>Title</th>
+						</tr>
+					</thead>
+					<tbody>
+						{posts.map((item) => (
+							<tr key={`${item.id}`}>
+								<td>{item.id}</td>
+								<td>{item.title}</td>
+							</tr>
+						))}
+					</tbody>
+				</Table>
+			</Container>
+		</div>
+	);
 }
 
 export default App;
